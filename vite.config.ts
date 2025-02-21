87ee1dc074e9b06ae286c9c5468d31c1767a043f
Saqlain Abbas <saqlain.razee@gmail.com>
2025-02-21 19:29:48 +0500

    Update vite config to prevent caching issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
